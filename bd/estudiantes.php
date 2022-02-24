fad1a4c9770715ac09462edaf22d0ed9da5b735c
Celucio <75482093+Celucio@users.noreply.github.com>
2022-02-23 19:55:49 -0500

    sada

--- bd/estudiantes.php
+++ bd/estudiantes.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../Estilos/s1.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estudiantes</title>
 </head>
+
 <body>
-<header>
+    <header>
         <nav>
             <a href="../S1.html">Semestre 1</a>
             <a href="../S2.html">Semestre 2</a>
@@ -34,6 +35,43 @@
         <div class="wave" style="height: 150px; overflow: hidden;">
             </svg></div>
     </header>
+    <br>
+    <br>
+    <center>
+        <div style="width: 50%; height: auto; background-color: rgb(102, 178, 255); border-radius: 20px;">
+            <h1>Formulario Estudiantes</h1>
+            <center>
+                <form method="POST" action="evaluaciones.php">
+                    <p>Nombre</p>
+                    <input type="text" name="nombre" placeholder="Nombre">
+                    <br>
+                    <p>Apellido</p>
+                    <input type="text" name="apellido" placeholder="Apellido">
+                    <br>
+                    <p>Edad</p>
+                    <input type="text" name="edad" placeholder="Edad">
+                    <br>
+                    <p>Telefono</p>
+                    <input type="text" name="telefono" placeholder="Telefono">
+                    <br>
+                    <p>Cedula</p>
+                    <input type="text" name="cedula" placeholder="Cedula">
+                    <br>
+                    <p>Dirección</p>
+                    <input type="text" name="dir" placeholder="Dirección">
+                    <br>
+                    <br>
+                    <input type="submit" value="Enviar" name="enviar">
+                    <input type="submit" value="Mostrar" name="mostrar">
+                    <br>
+                    <br>
+                </form>
+            </center>
+
+        </div>
+    </center>
+    <br>
+    <br>
     <footer>
         <div class="contenedor-footer">
             <div class="content-foo">
@@ -51,13 +89,12 @@
         </div>
         <h2 class="titulo-final">&copy; GRUPO 5 | Tecnologías Web</h2>
     </footer>
-    
+
 </body>
 <!-- Script de bootsatrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
-crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 <!-- Script de sweet alrt -->
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="/Javascript/datos8.js"></script>
+
 </html>
--- bd/evaluaciones.php
+++ bd/evaluaciones.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../Estilos/s1.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evaluaciones</title>
 </head>
+
 <body>
-<header>
+    <header>
         <nav>
             <a href="../S1.html">Semestre 1</a>
             <a href="../S2.html">Semestre 2</a>
@@ -34,6 +35,16 @@
         <div class="wave" style="height: 150px; overflow: hidden;">
             </svg></div>
     </header>
+    <div>
+        <h1>Formulario Evaluaciones</h1>
+        <center>
+            <form method="POST" action="evaluaciones.php">
+                <p></p>
+            </form>
+        </center>
+
+    </div>
+
     <footer>
         <div class="contenedor-footer">
             <div class="content-foo">
@@ -51,13 +62,12 @@
         </div>
         <h2 class="titulo-final">&copy; GRUPO 5 | Tecnologías Web</h2>
     </footer>
-    
+
 </body>
 <!-- Script de bootsatrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
-crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 <!-- Script de sweet alrt -->
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="/Javascript/datos8.js"></script>
+
 </html>
